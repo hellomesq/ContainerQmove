31244de452b6b27956935af1f32663e2cc62bfc1
hellomesq <helo.alvz@gmail.com>
2025-10-01 14:54:29 -0300

    att

--- obj/Debug/net9.0/MotoMonitoramento.AssemblyInfo.cs
+++ obj/Debug/net9.0/MotoMonitoramento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MotoMonitoramento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c33615e6ad418f67dcaefe01307ce9bf598755b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1b3ad2f6c41c93a9f7950bb2deda9785ae2ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MotoMonitoramento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MotoMonitoramento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
